c89ec6f588a4dc1fb6ba45f5bf141bb12ca496a9
yuanezra <bagasrayhan600@gmail.com>
2023-12-05 02:24:16 +0700

    Update Final v0.1.0

--- Assets/Script/QuizScript/QuizManager.cs
+++ Assets/Script/QuizScript/QuizManager.cs
@@ -6,24 +6,46 @@ using UnityEngine.UI;
 public class QuizManager : MonoBehaviour
 {
     [SerializeField] private MouseHover mouseHover;
+    int currentQuestion = 0;
     public List<QuestionAndAnswer> QnA;
+
+    void Start()
+    {
+        foreach (var question in QnA)
+        {
+            question.gameObject.SetActive(false);
+        }
+        QnA[currentQuestion].gameObject.SetActive(true);
+    }
 
     public void CheckAnswer(string answer)
     {
-        QnA.ForEach(qna => {
-            if (qna.Answer == answer)
-            {
-                mouseHover.IsQuizActive = false;
-                gameObject.SetActive(false);
-                Cursor.lockState = CursorLockMode.Locked;
-                return;
-            }
-        });
+        if (QnA[currentQuestion].Answer == answer)
+        {
+            mouseHover.IsQuizActive = false;
+            NextQuestion();
+        }
+
+        if (currentQuestion == QnA.Count - 1)
+        {
+            QnA[currentQuestion].gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            mouseHover.IsQuizActive = false;
+        }
     }
 
     public void NextQuestion()
     {
-        gameObject.SetActive(true);
+        if (currentQuestion < QnA.Count - 1)
+            StartCoroutine(ShowNextQuestionCoroutine());
     }
 
+    private IEnumerator ShowNextQuestionCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        QnA[currentQuestion].gameObject.SetActive(false);
+        currentQuestion++;
+        QnA[currentQuestion].gameObject.SetActive(true);
+    }
 }
